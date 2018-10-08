df0eb83478ecf8bb61f5323ae562b8439559e1f2
Michael Maxwell <mike.ian.maxwell@gmail.com>
2018-10-08 18:12:22 -0400

    main: added static cards to deck

--- src/main/java/core/Deck.java
+++ src/main/java/core/Deck.java
@@ -8,6 +8,62 @@ public class Deck {
 
 	private List<Card> cards;
 	
+	public static final Card CA = new Card("CA");
+	public static final Card C2 = new Card("C2");
+	public static final Card C3 = new Card("C3");
+	public static final Card C4 = new Card("C4");
+	public static final Card C5 = new Card("C5");
+	public static final Card C6 = new Card("C6");
+	public static final Card C7 = new Card("C7");
+	public static final Card C8 = new Card("C8");
+	public static final Card C9 = new Card("C9");
+	public static final Card C10 = new Card("C10");
+	public static final Card CJ = new Card("CJ");
+	public static final Card CQ = new Card("CQ");
+	public static final Card CK = new Card("CK");
+	
+	public static final Card DA = new Card("DA");
+	public static final Card D2 = new Card("D2");
+	public static final Card D3 = new Card("D3");
+	public static final Card D4 = new Card("D4");
+	public static final Card D5 = new Card("D5");
+	public static final Card D6 = new Card("D6");
+	public static final Card D7 = new Card("D7");
+	public static final Card D8 = new Card("D8");
+	public static final Card D9 = new Card("D9");
+	public static final Card D10 = new Card("D10");
+	public static final Card DJ = new Card("DJ");
+	public static final Card DQ = new Card("DQ");
+	public static final Card DK = new Card("DK");
+	
+	public static final Card HA = new Card("HA");
+	public static final Card H2 = new Card("H2");
+	public static final Card H3 = new Card("H3");
+	public static final Card H4 = new Card("H4");
+	public static final Card H5 = new Card("H5");
+	public static final Card H6 = new Card("H6");
+	public static final Card H7 = new Card("H7");
+	public static final Card H8 = new Card("H8");
+	public static final Card H9 = new Card("H9");
+	public static final Card H10 = new Card("H10");
+	public static final Card HJ = new Card("HJ");
+	public static final Card HQ = new Card("HQ");
+	public static final Card HK = new Card("HK");
+	
+	public static final Card SA = new Card("SA");
+	public static final Card S2 = new Card("S2");
+	public static final Card S3 = new Card("S3");
+	public static final Card S4 = new Card("S4");
+	public static final Card S5 = new Card("S5");
+	public static final Card S6 = new Card("S6");
+	public static final Card S7 = new Card("S7");
+	public static final Card S8 = new Card("S8");
+	public static final Card S9 = new Card("S9");
+	public static final Card S10 = new Card("S10");
+	public static final Card SJ = new Card("SJ");
+	public static final Card SQ = new Card("SQ");
+	public static final Card SK = new Card("SK");
+	
 	public Deck() {
 		cards = new ArrayList<>(52);
 		for (Rank r : Rank.values()) {
