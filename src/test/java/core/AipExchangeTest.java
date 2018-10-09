2b1c9d5e0b074d9fe9c42c3faaf7883b05805c63
Michael Maxwell <mike.ian.maxwell@gmail.com>
2018-10-09 02:43:07 -0400

    test: 1 away from a straight, many possible combinations

--- src/test/java/core/AipExchangeTest.java
+++ src/test/java/core/AipExchangeTest.java
@@ -56,6 +56,59 @@ public class AipExchangeTest extends TestCase {
 		assertTrue(ex.contains(Deck.H6));
 	}
 	
+	public void test1AwayStraight() {
+		Hand seq4x = new Hand("D4", "D5", "H6", "C7", "H10");
+		List<Card> e4x = seq4x.exchange();
+		System.out.println(e4x);
+		assertEquals(1, e4x.size());
+		assertTrue(e4x.contains(Deck.H10));
+		
+		Hand seq1x3 = new Hand("C4", "H9", "HJ", "CQ", "HK");
+		List<Card> e1x3 = seq1x3.exchange();
+		assertEquals(1, e1x3.size());
+		assertTrue(e1x3.contains(Deck.C4));
+		
+		Hand seq3x1 = new Hand("C4", "H5", "H6", "C8", "H10");
+		List<Card> e3x1 = seq3x1.exchange();
+		assertEquals(1, e3x1.size());
+		assertTrue(e3x1.contains(Deck.H10)); // or c4
+		
+		Hand seqx4 = new Hand("C4", "H5", "H6", "C8", "H9");
+		List<Card> ex4 = seqx4.exchange();
+		assertEquals(1, ex4.size());
+		assertTrue(ex4.contains(Deck.H9)); // or c4
+		
+		Hand seqPair = new Hand("C4", "H5", "H6", "C6", "H7");
+		List<Card> ePair = seqPair.exchange();
+		assertEquals(1, ePair.size());
+		assertTrue(ePair.contains(Deck.C6)); // or h6
+		
+		Hand seqLowA = new Hand("CA", "H2", "H3", "C4", "H10");
+		List<Card> eLowA = seqLowA.exchange();
+		assertEquals(1, eLowA.size());
+		assertTrue(eLowA.contains(Deck.H10));
+		
+		Hand seqHighA = new Hand("CA", "HK", "H6", "CJ", "H10");
+		List<Card> eHighA = seqHighA.exchange();
+		assertEquals(1, eHighA.size());
+		assertTrue(eHighA.contains(Deck.H6));
+		
+		Hand seqLow = new Hand("C2", "H3", "H4", "C5", "H10");
+		List<Card> eLow = seqLow.exchange();
+		assertEquals(1, eLow.size());
+		assertTrue(eLow.contains(Deck.H10));
+		
+		Hand seqHigh = new Hand("CK", "HQ", "HJ", "C6", "H10");
+		List<Card> eHigh = seqHigh.exchange();
+		assertEquals(1, eHigh.size());
+		assertTrue(eHigh.contains(Deck.C6));
+		
+		Hand seq4 = new Hand("C5", "H6", "HA", "C8", "H9");
+		List<Card> e4 = seq4.exchange();		
+		assertEquals(1, e4.size());
+		assertTrue(e4.contains(Deck.HA));
+	}
+	
 //	public void test1AwayFullHouse() {
 //		List<Card> ex2 = HandTest.twoPair.exchange();
 //		List<Card> ex3 = HandTest.threeOfKind.exchange();
